e521fcb773cf18acf80e2d793cd6e1287baf99ed
Mary Selig <seligmar@gmail.com>
2018-11-05 21:50:13 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
   
   def won?(board)
     WIN_COMBINATIONS.each do |winning|
-      if board.include?(winning)
+      if board.include?(winning) =
         puts "#{winning}"
       else 
         return false 
